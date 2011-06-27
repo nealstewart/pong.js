b2361f842fb064f6c574f4eb383bb24e84639d16
Neal Stewart <neal@liquidmedia.ca>
2011-06-27 15:11:09 -0300

    Made README semi-useful

--- app.js
+++ app.js
@@ -13,61 +13,6 @@ app.set('view options', {
 var io = require('socket.io').listen(app);
 var Room = require('room').Room;
 
-/*
-  ok so here's how this shit is going to work out.
-
-  i'm going to have a collection of rooms. each room can be joined by two players.
-
-  when you first load up the app there's a big old button that just says "start playing".
-
-  when you click that shit it brings up prompts for you to select the name of the room you'd like to join, as well as a username.
-
-  i'll assert uniqueness of usernames.
-
-  Once you log in shit looks like this:
-
-    Player 1              Player 2
-    Bob                   George
-    _______               _______
-    |Ready|               |Ready|
-    -------               -------
-   _________________________________
-                                  
-     |             *             |
-
-
-   _________________________________
-
-
-
-   Once both players click their ready buttons, the game is underway.
-
-   Runs to ten points. After which a player is declared the winner.
-
-   I'm going to run the game logic on the server.
-
-   So every game 'tick', the server is going to send the game state to the client.
-
-   which means the client basically looks like this
-
-   socket.on('gameTick', function(newGameState) {
-     drawGameState(newGameState);
-   });
-    
-   plus a couple handlers
-    
-   on keyDown for down arrow
-   socket.emit('requestMove', 'down');
-   on keyUp for down arrow
-   socket.emit('doneMove', 'down');
-
-   on keyDown for up arrow
-   socket.emit('requestMove', "up");
-   on keyUp for down arrow
-   socket.emit('doneMovingDown');
-  */
-
-
 io.sockets.on('connection', function(socket) {
   var room;
   socket.on('room-connect', function(info) {
