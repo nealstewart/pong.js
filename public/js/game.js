735d1a5595e0884b94dfd5659c4a3ee70f0864ec
Neal Stewart <neal@liquidmedia.ca>
2011-06-27 21:09:36 -0300

    bit more oo

--- public/js/game.js
+++ public/js/game.js
@@ -7,63 +7,209 @@ var currentPlayer;
 var countdownStarted = false;
 var otherPlayerReadyToPlay = false;
 
-var socket = io.connect('http://localhost');
+function GameState() {
+  var xDecider = Math.floor(Math.random() * 2);
+  var xDirection;
+  if (xDecider === 0) {
+    xDirection = LEFT;
+  } else {
+    xDirection = RIGHT;
+  }
 
-(function setupSocket(socket) {
-  var setPlayer = function(playerEl, userName) {
-    playerEl.find('button').remove();
-    playerEl.find('span').remove();
+  var yVelocity = 3;
+  var xVelocity = xDirection * 10;
 
-    if (userName) {
-      playerEl.append('<span>'+userName+'</span>');
-    } else {
-      playerEl.append('<button class=join>Join</button>');
-      if (currentPlayer) {
-        playerEl.find('button').attr('disabled', 'disabled');
+  this.ball = {
+    x : canvasWidth() / 2 - (BALL_DIAMETER / 2),
+    y : canvasHeight() / 2 - (BALL_DIAMETER / 2),
+    velocity : {
+      x : xVelocity,
+      y : yVelocity
+    }
+  };
+
+  var paddleStartingY = canvasHeight() / 2 - (PADDLE_HEIGHT / 2);
+
+  this.leftPlayer = { y : paddleStartingY };
+  this.rightPlayer = { y : paddleStartingY };
+
+  return this;
+}
+
+GameState.prototype = {
+  tick : function() {
+    this.moveBall();
+  },
+
+  moveBall : function() {
+    this.ball.x = this.ball.x + this.ball.velocity.x;
+    this.ball.y = this.ball.y + this.ball.velocity.y;
+
+    var leftEdge = this.ball.x,
+        rightEdge = this.ball.x + BALL_DIAMETER,
+        top = this.ball.y,
+        bottom = this.ball.y + BALL_DIAMETER;
+
+    if (leftEdge < 0) {
+      this.ball.x = 0;
+      this.ball.velocity.x = -this.ball.velocity.x;
+    } else if (rightEdge > canvasWidth()) {
+      this.ball.x = canvasWidth() - BALL_DIAMETER;
+      this.ball.velocity.x = -this.ball.velocity.x;
+    }
+
+    if (top < 0) {
+      this.ball.y = 0;
+      this.ball.velocity.y = -this.ball.velocity.y;
+    } else if (bottom > canvasHeight()) {
+      this.ball.y = canvasHeight() - BALL_DIAMETER;
+      this.ball.velocity.y = -this.ball.velocity.y;
+    }
+  },
+
+  toJSON : function() {
+    return {
+      ball: this.ball,
+      leftPlayer : this.leftPlayer,
+      rightPlayer : this.rightPlayer
+    };
+  }
+};
+
+var Game = function() {
+  this.socket = io.connect('http://localhost');
+  this.otherPlayerReadyToPlay = false;
+
+  _.bindAll(this, 'tick', 'startGame');
+
+  this.setupSocket(this.socket);
+};
+
+Game.prototype = {
+  startGame : function() {
+    this.gameState = new GameState();
+    this.socket.emit('game-start', this.gameState);
+
+    setTimeout(this.tick, 1000 / 60);
+  },
+
+  tick : function() {
+    this.socket.emit('game-tick', this.gameState.toJSON());
+
+    this.gameState.tick();
+
+    setTimeout(this.tick, 1000 / 60);
+  },
+
+  setupSocket : function(socket) {
+    var game = this; 
+
+    var setPlayer = function(playerEl, userName) {
+      playerEl.find('button').remove();
+      playerEl.find('span').remove();
+
+      if (userName) {
+        playerEl.append('<span>'+userName+'</span>');
+      } else {
+        playerEl.append('<button class=join>Join</button>');
+        if (game.currentPlayer) {
+          playerEl.find('button').attr('disabled', 'disabled');
+        }
       }
+    };
+
+    var setPlayers = function(playerInfo) {
+      var player1El = $('.player_1');
+      var player2El = $('.player_2');
+
+      setPlayer(player1El, playerInfo.player1);
+      setPlayer(player2El, playerInfo.player2);
+
+      currentRoster = playerInfo;
+    };
+
+    this.socket.on('game-start', function(gameState) {
+      hideCountdown();
+      drawGameState(gameState);
+    });
+
+    this.socket.on('game-tick', function(gameState) {
+      drawGameState(gameState);
+    });
+
+    this.socket.on('player-roster', function(info) {
+      setPlayers(info);
+
+      var twoPlayers = (currentRoster.player1 && currentRoster.player2);
+      
+      if (twoPlayers) { showStartButton(); }
+    });
+
+    this.socket.on('room-connected', function(info) {
+      game.currentPlayer = info;
+      disableJoinButtons();
+    });
+
+    this.socket.on('player-ready', function(playerNumber) {
+      game.otherPlayerReadyToPlay = true;
+      if (playerNumber === game.currentPlayer.number) {
+        $('button.start').text('Ready');
+      }
+    });
+
+    this.socket.on('game-countdown', drawCountdown);
+  },
+
+  countdown : function() {
+    var currentCount = 3;
+    console.log("countdown was called");
+
+    var _countdown = function() {
+      console.log("_countdown was called");
+      debugger;
+      this.socket.emit('game-countdown', currentCount);
+
+      if (currentCount > 0) {
+        setTimeout(_countdown, 1000);
+      } else {
+        setTimeout(this.startGame, 500);
+      }
+
+      currentCount -= 1;
+    };
+
+    _countdown = _.bind(_countdown, this);
+
+    setTimeout(_countdown, 1000);
+  },
+
+  registerAsReady : function() {
+    if (this.otherPlayerReadyToPlay) {
+      this.socket.emit('player-ready');
+      this.countdown();
+    } else {
+      this.socket.emit('player-ready');
     }
-  };
+  },
 
-  var setPlayers = function(playerInfo) {
-    var player1El = $('.player_1');
-    var player2El = $('.player_2');
+  joinAs : function(playerNumber) {
+    var userName = prompt("Enter your name:");
 
-    setPlayer(player1El, playerInfo.player1);
-    setPlayer(player2El, playerInfo.player2);
+    if (userName) $.trim(userName);
+    if (!userName) {
+      return;
+    } else {
+      var roomName = $('body').data('room-name');
 
-    currentRoster = playerInfo;
-  };
-
-  socket.on('game-start', function(gameState) {
-    hideCountdown();
-    drawGameState(gameState);
-  });
-
-  socket.on('game-tick', function(gameState) {
-    drawGameState(gameState);
-  });
-
-  socket.on('player-roster', function(info) {
-    setPlayers(info);
-
-    var twoPlayers = (currentRoster.player1 && currentRoster.player2);
-    
-    if (twoPlayers) { showStartButton(); }
-  });
-
-  socket.on('room-connected', function(info) {
-    currentPlayer = info;
-    disableJoinButtons();
-  });
-
-  socket.on('player-ready', function(playerNumber) {
-    otherPlayerReadyToPlay = true;
-    if (playerNumber === currentPlayer.number) {
-      $('button.start').text('Ready');
+      this.socket.emit('room-connect', {
+        playerNumber : playerNumber, 
+        roomName : roomName,
+        userName : userName
+      });
     }
-  });
-  socket.on('game-countdown', drawCountdown);
-})(socket);
+  }
+};
+
 
 function disableJoinButtons() {
   $('.players button').attr('disabled', 'disabled');
@@ -173,144 +319,19 @@ function canvasWidth() {
   return width;
 }
 
-function GameState() {
-  var xDecider = Math.floor(Math.random() * 2);
-  var xDirection;
-  if (xDecider === 0) {
-    xDirection = LEFT;
-  } else {
-    xDirection = RIGHT;
-  }
-
-  var yVelocity = 3;
-  var xVelocity = xDirection * 10;
-
-  this.ball = {
-    x : canvasWidth() / 2 - (BALL_DIAMETER / 2),
-    y : canvasHeight() / 2 - (BALL_DIAMETER / 2),
-    velocity : {
-      x : xVelocity,
-      y : yVelocity
-    }
-  };
-
-  _.bindAll(this, 'tick');
-
-  var paddleStartingY = canvasHeight() / 2 - (PADDLE_HEIGHT / 2);
-
-  this.leftPlayer = { y : paddleStartingY };
-  this.rightPlayer = { y : paddleStartingY };
-
-  return this;
-}
-
-GameState.prototype = {
-  tick : function() {
-    this.moveBall();
-    socket.emit('game-tick', this.toJSON());
-
-    setTimeout(this.tick, 1000 / 60);
-  },
-
-  moveBall : function() {
-    this.ball.x = this.ball.x + this.ball.velocity.x;
-    this.ball.y = this.ball.y + this.ball.velocity.y;
-
-    var leftEdge = this.ball.x,
-        rightEdge = this.ball.x + BALL_DIAMETER,
-        top = this.ball.y,
-        bottom = this.ball.y + BALL_DIAMETER;
-
-    if (leftEdge < 0) {
-      this.ball.x = 0;
-      this.ball.velocity.x = -this.ball.velocity.x;
-    } else if (rightEdge > canvasWidth()) {
-      this.ball.x = canvasWidth() - BALL_DIAMETER;
-      this.ball.velocity.x = -this.ball.velocity.x;
-    }
-
-    if (top < 0) {
-      this.ball.y = 0;
-      this.ball.velocity.y = -this.ball.velocity.y;
-    } else if (bottom > canvasHeight()) {
-      this.ball.y = canvasHeight() - BALL_DIAMETER;
-      this.ball.velocity.y = -this.ball.velocity.y;
-    }
-  },
-
-  toJSON : function() {
-    return {
-      ball: this.ball,
-      leftPlayer : this.leftPlayer,
-      rightPlayer : this.rightPlayer
-    };
-  }
-};
-
-var currentGameState;
-function startGame() {
-  currentGameState = new GameState();
-
-  socket.emit('game-start', currentGameState);
-
-  setTimeout(currentGameState.tick, 1000 / 60);
-}
-
-function countdown() {
-  var currentCount = 3;
-
-  function _countdown() {
-    socket.emit('game-countdown', currentCount);
-
-    if (currentCount > 0) {
-      setTimeout(_countdown, 1000);
-    } else {
-      setTimeout(startGame, 500);
-    }
-
-    currentCount -= 1;
-  }
-
-  setTimeout(_countdown, 1000);
-}
-
-function registerAsReady() {
-  if (otherPlayerReadyToPlay) {
-    socket.emit('player-ready');
-    countdown();
-  } else {
-    socket.emit('player-ready');
-  }
-}
-
-function joinAs(playerNumber) {
-  var userName = prompt("Enter your name:");
-
-  if (userName) $.trim(userName);
-  if (!userName) {
-    return;
-  } else {
-    var roomName = $('body').data('room-name');
-
-    socket.emit('room-connect', {
-      playerNumber : playerNumber, 
-      roomName : roomName,
-      userName : userName
-    });
-  }
-}
-
 $(function() {
+  var game = new Game();
+
   $('.player_1 .join').click(function() {
-    joinAs(PLAYER_1);
+    game.joinAs(PLAYER_1);
   });
 
   $('.player_2 .join').click(function() {
-    joinAs(PLAYER_2);
+    game.joinAs(PLAYER_2);
   });
 
   $('button.start').click(function() {
-    registerAsReady();
+    game.registerAsReady();
   });
 });
 })();
